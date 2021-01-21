93c80d1d6713e2464c4097fbc77e877b236547c4
LeenaJaswani <leena.jaswani@gmail.com>
2021-01-21 18:21:15 +0530

    tweet

--- tweets/validators.py
+++
@@ -1,8 +0,0 @@
-from django.core.exceptions import ValidationError
-
-# Create your models here.
-def validate_content(value):
-		content=value
-		if content=="":
-			raise ValidationError("Cannot be blank")
-		return value
